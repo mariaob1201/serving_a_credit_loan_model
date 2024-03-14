23c16f5a145abf741d99c4d885508de64efa6bed
Maria de Lourdes Oros Barrón <mariaob1201>
2024-03-13 22:18:21 -0500

    CSS style

--- app.py
+++ app.py
@@ -72,33 +72,6 @@ def main():
     st.image(image_path, caption='CreditN', width=100)
 
     # Link to external CSS file
-    css_styles = """
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f0f0f0;
-                color: #333;
-            }
-
-            .container {
-                max-width: 800px;
-                margin: 0 auto;
-                padding: 20px;
-                background-color: #fff;
-                border-radius: 10px;
-                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-            }
-
-            h1 {
-                font-size: 28px;
-                font-weight: bold;
-                color: #007bff;
-                margin-bottom: 20px;
-            }
-        </style>
-        """
-
-    #st.markdown(css_styles, unsafe_allow_html=True)
     with open('styles.css', 'r') as css_file:
         css_styles = css_file.read()
 
