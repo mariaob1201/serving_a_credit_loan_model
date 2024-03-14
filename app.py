8c071c081b5c690204e84de60b3a83eb36e67bd9
Maria de Lourdes Oros Barrón <mariaob1201>
2024-03-13 22:13:29 -0500

    ADD CSS style

--- app.py
+++ app.py
@@ -73,31 +73,31 @@ def main():
 
     # Link to external CSS file
     css_styles = """
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
-            color: #333;
-        }
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f0f0f0;
+                color: #333;
+            }
 
-        .container {
-            max-width: 800px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 10px;
-            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-        }
+            .container {
+                max-width: 800px;
+                margin: 0 auto;
+                padding: 20px;
+                background-color: #fff;
+                border-radius: 10px;
+                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            }
 
-        h1 {
-            font-size: 28px;
-            font-weight: bold;
-            color: #007bff;
-            margin-bottom: 20px;
-        }
-
+            h1 {
+                font-size: 28px;
+                font-weight: bold;
+                color: #007bff;
+                margin-bottom: 20px;
+            }
+        </style>
         """
 
-    # Embed CSS styles using HTML
     st.markdown(f"<style>{css_styles}</style>", unsafe_allow_html=True)
 
     # User input for name, address, and purpose
